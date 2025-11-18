dd6dac2bd6e4cb90385865bf3afb626587654280
Emiliano Debenedetti <emilianodebenedetti@outlook.com>
2025-11-17 23:20:12 -0300

    Modifico cors

--- src/server.js
+++ src/server.js
@@ -23,11 +23,11 @@ const corsOptions = {
   ] ,
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
+  allowedHeaders: ["Content-Type", "Authorization","Access-Control-Allow-Origin" ],
 };
 app.use(cors(corsOptions));
 
-app.options('*', cors(corsOptions));
+app.options('/.*/', cors(corsOptions));
 
 app.use(express.json());
 
