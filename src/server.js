801be3a51ec1ee1d48b22a6401ff159faa4eb1f0
Emiliano Debenedetti <emilianodebenedetti@outlook.com>
2026-01-07 18:23:48 -0300

    fix corsOptions prod

--- src/server.js
+++ src/server.js
@@ -33,8 +33,8 @@ const corsOptions = {
 app.use(cors(corsOptions));
 /* app.options('*', cors(corsOptions)); */
 
-//app.options('/.*/', cors(corsOptions)); //prodcuccion
-app.options("", cors(corsOptions)); //desarrollo
+app.options('/.*/', cors(corsOptions)); //prodcuccion
+//app.options("", cors(corsOptions)); //desarrollo
 
 
 /* const corsOptions = {
