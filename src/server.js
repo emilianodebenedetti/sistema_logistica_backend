e22765e68b2606aba4cfdf817e3a48cf00760e5a
Emiliano Debenedetti <emilianodebenedetti@outlook.com>
2026-01-07 14:19:29 -0300

    cors produccion

--- src/server.js
+++ src/server.js
@@ -29,10 +29,10 @@ const corsOptions = {
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
 };
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
 
-/* app.use(cors(corsOptions)); */
-/* app.options('*', cors(corsOptions)); */
-app.options('/.*/', cors(corsOptions)); //prodcuccion
+//app.options('/.*/', cors(corsOptions)); //prodcuccion
 /* app.options("", cors(corsOptions)); */ //desarrollo
 
 /* const corsOptions = { 
