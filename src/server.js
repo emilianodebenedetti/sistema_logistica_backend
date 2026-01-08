acef7afcc1901a2e3b0edd33acf43bf79ddfea6b
Emiliano Debenedetti <emilianodebenedetti@outlook.com>
2026-01-07 21:04:13 -0300

    agrego Access-Control-Allow-Origin

--- src/server.js
+++ src/server.js
@@ -20,7 +20,7 @@ const corsOptions = {
     : ["http://localhost:5173"],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
+  allowedHeaders: ["Content-Type", "Authorization", "Access-Control-Allow-Origin"],
 };
 app.use(cors(corsOptions));
 //app.options('/.*/', cors(corsOptions));//prod
