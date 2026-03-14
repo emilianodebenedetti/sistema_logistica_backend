943de6308b1d612c99dc00413a7073b9bfc47709
Emiliano Debenedetti <emilianodebenedetti@outlook.com>
2026-03-13 22:00:30 -0300

    Habilito choferes a listar loss clientes

--- src/routes/clientes.routes.js
+++ src/routes/clientes.routes.js
@@ -6,7 +6,7 @@ const router = Router();
 
 // Solo admin puede manejar clientes
 router.post('/', verificarToken, esAdmin, crearCliente); //testeado admin y chofer (no puede)
-router.get('/', verificarToken, esAdmin, listarClientes); //testeado admin y chofer (no puede)
+router.get('/', verificarToken, listarClientes); //testeado admin y chofer (no puede)
 router.delete('/:id', verificarToken, esAdmin, eliminarCliente); //testeado admin y chofer (no puede)
 
 export default router;
